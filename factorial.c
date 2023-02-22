3d60b33954a6a8008ae83a6e9a507983ac6e165a
el898 <ebubeexcel3@gmail.com>
2023-02-22 10:29:01 +0100

    task 0

---
+++ factorial.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * factorial - calculates the factorial of a given integer n
+ * @n: integer argument for the program
+ *
+ * Return: factorial of n, or -1 if n is negative
+ */
+int factorial(int n)
+{
+	int result = 1;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		result *= i;
+	}
+
+	return (result);
+}
+
